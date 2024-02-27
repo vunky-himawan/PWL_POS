8f56b20a10e2a55b43c1e000bd3fb8337a06150a
Vunky Himawan <tingkerbeii44@gmail.com>
2024-02-27 14:15:51 +0700

    Praktikum 6 - Implementasi Eloquent ORM

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        // $data = [
+        //     'username' => "customer-1",
+        //     'nama' => "Pelanggan",
+        //     "password" => Hash::make('12345'),
+        //     "level_id" => 3
+        // ];
+        // UserModel::insert($data);
+        $data = [
+            'nama' => "Pelanggan Pertama"
+        ];
+        UserModel::where('username', 'customer-1')->update($data);
+
+
+        $users = UserModel::all();
+        return view('user', ['datas' => $users]);
+    }
+}
---
+++ resources/views/user.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Data User</title>
+</head>
+
+<body>
+    <h1>Data User</h1>
+    <table border="1" cellpadding="2" cellspacing="0">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Nama</th>
+                <th>ID Level Pengguna</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($datas as $data)
+                <tr>
+                    <td>{{ $data->user_id }}</td>
+                    <td>{{ $data->username }}</td>
+                    <td>{{ $data->nama }}</td>
+                    <td>{{ $data->level_id }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+
+</html>
