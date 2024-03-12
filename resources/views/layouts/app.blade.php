517f5167ab40c6739e8a6d1697d06ded0074e8d1
Vunky Himawan <tingkerbeii44@gmail.com>
2024-03-12 15:42:35 +0700

    Praktikum 2 - Integrasi dengan DataTables

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,8 @@
     @endif
 @stop
 
+@vite('resources/js/app.js')
+
 {{-- Extend and customize the page content header --}}
 @section('content_header')
     @hasSection('content_header_title')
@@ -42,24 +44,23 @@
 
 {{-- Add common Javascript/Jquery code --}}
 @push('js')
-    <script>
-        $(document).ready(function() {
-            // Add your common script logic here...
-        });
-    </script>
+    <script src="https://cdn.datatables.net/2.0.2/js/dataTables.js"></script>
 @endpush
+
+@stack('scripts')
 
 {{-- Add common CSS customizations --}}
 @push('css')
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.2/css/dataTables.dataTables.css" />
     <style type="text/css">
         {{-- You can add AdminLTE customizations here --}}
         /*
-                        .card-header {
-                        border-bottom: none;
-                        }
-                        .card-title {
-                        font-weight: 600;
-                        }
-                        */
+        .card-header {
+        border-bottom: none;
+        }
+        .card-title {
+        font-weight: 600;
+        }
+        */
     </style>
 @endpush
